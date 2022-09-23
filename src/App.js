f1aa75d6f14d56136b77be998e6d9767690d3981
Tianye Chen <78762926+tianye-chen@users.noreply.github.com>
2022-09-23 17:20:29 -0400

    Complete

--- src/App.js
+++ src/App.js
@@ -14,37 +14,49 @@ function App() {
     setSelectedAnswer(selection);
   };
 
+  const handleClick = () => {
+      fetch("https://opentdb.com/api.php?amount=1&category=9&type=multiple")
+          .then((res) => res.json())
+          .then((data) => {
+              console.log(data.results[0]);
+              setQuestionData(data.results[0]);
+              setSelectedAnswer(null);
+          });
+  };
+
   let card;
 
   if (selectedAnswer) {
-    card = (
-      <ResultCard
-        correct={selectedAnswer === questionData.correct_answer}
-        answer={questionData.correct_answer}
-      />
-    );
+      card = (
+          <ResultCard
+              correct={selectedAnswer === questionData.correct_answer}
+              answer={questionData.correct_answer}
+          />
+      );
   } else {
-    let options = [
-      questionData.correct_answer,
-      ...questionData.incorrect_answers,
-    ];
-    card = (
-      <QuestionCard
-        question={questionData.question}
-        options={shuffleArray(options)}
-        selectAnswer={selectAnswer}
-      />
-    );
+      let options = [
+          questionData.correct_answer,
+          ...questionData.incorrect_answers,
+      ];
+      card = (
+          <QuestionCard
+              question={questionData.question}
+              options={shuffleArray(options)}
+              selectAnswer={selectAnswer}
+          />
+      );
   }
 
   return (
-    <div className="w-100 my-5 d-flex justify-content-center align-items-center">
-      <div style={{ maxWidth: "45%" }}>
-        <h1 className="text-center">Trivia App</h1>
-        <button className="btn btn-success">Next Question</button>
-        {card}
+      <div className="w-100 my-5 d-flex justify-content-center align-items-center">
+          <div style={{ maxWidth: "45%" }}>
+              <h1 className="text-center">Trivia App</h1>
+              <button className="btn btn-success" onClick={handleClick}>
+                  Next Question
+              </button>
+              {card}
+          </div>
       </div>
-    </div>
   );
 }
 
