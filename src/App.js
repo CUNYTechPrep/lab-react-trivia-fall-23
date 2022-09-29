7ec022db1cb5918e48155b5059030017a39a753a
Abdul Rafi <arafi6624@bths.edu>
2022-09-29 16:32:08 -0400

    Trivia Lab Completed

--- src/App.js
+++ src/App.js
@@ -14,6 +14,20 @@ function App() {
     setSelectedAnswer(selection);
   };
 
+  const getQuestion = (event) => {
+
+    const fetchPromise = fetch("https://opentdb.com/api.php?amount=1&category=9&type=multiple");
+
+    fetchPromise
+      .then((response) => response.json())
+      .then((body) => {
+        setQuestionData(body.results[0]);
+        setSelectedAnswer(null);
+      })
+      .catch((error) => {console.error('Error: ', error)});
+    
+  };
+
   let card;
 
   if (selectedAnswer) {
@@ -41,7 +55,7 @@ function App() {
     <div className="w-100 my-5 d-flex justify-content-center align-items-center">
       <div style={{ maxWidth: "45%" }}>
         <h1 className="text-center">Trivia App</h1>
-        <button className="btn btn-success">Next Question</button>
+        <button className="btn btn-success" onClick={getQuestion}>Next Question</button>
         {card}
       </div>
     </div>
