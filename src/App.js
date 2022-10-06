f4621627d5554f897a33ede57ba71f40af804baa
Ivan <ivanrao@outlook.com>
2022-10-06 19:47:18 -0400

    finished lab

--- src/App.js
+++ src/App.js
@@ -7,45 +7,61 @@ import rawTriviaQuestion from "./lib/data";
 const triviaQuestion = rawTriviaQuestion.results[0];
 
 function App() {
-  const [selectedAnswer, setSelectedAnswer] = useState(null);
-  const [questionData, setQuestionData] = useState(triviaQuestion);
+	const [selectedAnswer, setSelectedAnswer] = useState(null);
+	const [questionData, setQuestionData] = useState(triviaQuestion);
 
-  const selectAnswer = (selection) => {
-    setSelectedAnswer(selection);
-  };
+	const selectAnswer = (selection) => {
+		setSelectedAnswer(selection);
+	};
 
-  let card;
+	const getNextQuestion = async () => {
+		try {
+			const response = await fetch(
+				"https://opentdb.com/api.php?amount=1&category=9&type=multiple"
+			);
+			const data = await response.json();
+			const questionData = await data.results[0];
+			setQuestionData(questionData);
+			setSelectedAnswer(null);
+		} catch (error) {
+			console.log("Error: ", error);
+		}
+	};
 
-  if (selectedAnswer) {
-    card = (
-      <ResultCard
-        correct={selectedAnswer === questionData.correct_answer}
-        answer={questionData.correct_answer}
-      />
-    );
-  } else {
-    let options = [
-      questionData.correct_answer,
-      ...questionData.incorrect_answers,
-    ];
-    card = (
-      <QuestionCard
-        question={questionData.question}
-        options={shuffleArray(options)}
-        selectAnswer={selectAnswer}
-      />
-    );
-  }
+	let card;
 
-  return (
-    <div className="w-100 my-5 d-flex justify-content-center align-items-center">
-      <div style={{ maxWidth: "45%" }}>
-        <h1 className="text-center">Trivia App</h1>
-        <button className="btn btn-success">Next Question</button>
-        {card}
-      </div>
-    </div>
-  );
+	if (selectedAnswer) {
+		card = (
+			<ResultCard
+				correct={selectedAnswer === questionData.correct_answer}
+				answer={questionData.correct_answer}
+			/>
+		);
+	} else {
+		let options = [
+			questionData.correct_answer,
+			...questionData.incorrect_answers,
+		];
+		card = (
+			<QuestionCard
+				question={questionData.question}
+				options={shuffleArray(options)}
+				selectAnswer={selectAnswer}
+			/>
+		);
+	}
+
+	return (
+		<div className="w-100 my-5 d-flex justify-content-center align-items-center">
+			<div style={{ maxWidth: "45%" }}>
+				<h1 className="text-center">Trivia App</h1>
+				<button className="btn btn-success" onClick={getNextQuestion}>
+					Next Question
+				</button>
+				{card}
+			</div>
+		</div>
+	);
 }
 
 export default App;
