e478f78fd57817f326974f3b7a74a8a90779c151
ravenclaw1999 <702schowdhury@gmail.com>
2022-10-07 16:38:31 -0400

    completed trivia app

--- src/App.js
+++ src/App.js
@@ -14,6 +14,17 @@ function App() {
     setSelectedAnswer(selection);
   };
 
+  const handleClick = () => {
+    console.log("next question clicked");
+    fetch("https://opentdb.com/api.php?amount=1&category=9&type=multiple")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      setQuestionData(data.results[0]);
+      setSelectedAnswer(null);
+    })
+  }
+
   let card;
 
   if (selectedAnswer) {
@@ -41,7 +52,7 @@ function App() {
     <div className="w-100 my-5 d-flex justify-content-center align-items-center">
       <div style={{ maxWidth: "45%" }}>
         <h1 className="text-center">Trivia App</h1>
-        <button className="btn btn-success">Next Question</button>
+        <button onClick={handleClick} className="btn btn-success">Next Question</button>
         {card}
       </div>
     </div>
