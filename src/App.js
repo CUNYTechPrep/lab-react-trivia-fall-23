1be34d12748525e9a583cc7f0f2e6cf241a022e9
Kristina <kristinkor@gmail.com>
2022-10-13 23:20:18 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -10,17 +10,51 @@ function App() {
   const [selectedAnswer, setSelectedAnswer] = useState(null);
   const [questionData, setQuestionData] = useState(triviaQuestion);
 
-  const selectAnswer = (selection) => {
-    setSelectedAnswer(selection);
-  };
+  // state={
+  //   testCore:10
+  // }
 
-  let card;
+
+  var counter=0;
+
+  const getQuestion =() =>{
+    
+    fetch("https://opentdb.com/api.php?amount=1&category=9&type=multiple")
+    .then((res)=> res.json())
+    .then((body)=>{
+      setQuestionData(body.results[0]);
+      setSelectedAnswer(null);
+      // setTotal((prevTotal => prevTotal + 1).bind(this), 1000)
+     
+      // if(selectedAnswer === questionData.correct_answer){
+      //   counter+=1;
+      //   console.log(selectedAnswer === questionData.correct_answer)
+      // }
+
+    });
+    
+
+    };
+
+    const selectAnswer = (selection) => {
+      setSelectedAnswer(selection);
+     
+    };
+  
+    // const [total, setTotal] = React.useState(1)
+   
+    let card;
+
+   
 
   if (selectedAnswer) {
+   
+    
     card = (
       <ResultCard
         correct={selectedAnswer === questionData.correct_answer}
         answer={questionData.correct_answer}
+
       />
     );
   } else {
@@ -36,13 +70,14 @@ function App() {
       />
     );
   }
-
+  
   return (
     <div className="w-100 my-5 d-flex justify-content-center align-items-center">
       <div style={{ maxWidth: "45%" }}>
         <h1 className="text-center">Trivia App</h1>
-        <button className="btn btn-success">Next Question</button>
+        <button className="btn btn-success" onClick={getQuestion}>Next Question</button>
         {card}
+   
       </div>
     </div>
   );
