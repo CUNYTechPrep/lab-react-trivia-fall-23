b2ccb414e95b19a1a80d99ecb09cdc8830b8ad36
Kristina <kristinkor@gmail.com>
2022-10-19 06:26:55 +0300

    Add files via upload
    Added count but need to be corrected

--- src/App.js
+++ src/App.js
@@ -16,20 +16,24 @@ function App() {
 
 
   var counter=0;
-
+  let [total, setTotal] = React.useState(1)
+  let [score, setScore] = React.useState(0)
   const getQuestion =() =>{
     
     fetch("https://opentdb.com/api.php?amount=1&category=9&type=multiple")
     .then((res)=> res.json())
     .then((body)=>{
+      if(selectedAnswer === questionData.correct_answer){
+        setScore(score +1)
+       
+        console.log(selectedAnswer === questionData.correct_answer)
+      }
+      setTotal(total+1)
       setQuestionData(body.results[0]);
       setSelectedAnswer(null);
-      // setTotal((prevTotal => prevTotal + 1).bind(this), 1000)
-     
-      // if(selectedAnswer === questionData.correct_answer){
-      //   counter+=1;
-      //   console.log(selectedAnswer === questionData.correct_answer)
-      // }
+           
+
+
 
     });
     
@@ -41,7 +45,7 @@ function App() {
      
     };
   
-    // const [total, setTotal] = React.useState(1)
+
    
     let card;
 
@@ -54,6 +58,8 @@ function App() {
       <ResultCard
         correct={selectedAnswer === questionData.correct_answer}
         answer={questionData.correct_answer}
+        score = {score}
+        total = {total}
 
       />
     );
@@ -75,9 +81,9 @@ function App() {
     <div className="w-100 my-5 d-flex justify-content-center align-items-center">
       <div style={{ maxWidth: "45%" }}>
         <h1 className="text-center">Trivia App</h1>
-        <button className="btn btn-success" onClick={getQuestion}>Next Question</button>
+       
         {card}
-   
+        <button className="btn btn-success mt-4 w-100" onClick={getQuestion}>Next Question</button>
       </div>
     </div>
   );
