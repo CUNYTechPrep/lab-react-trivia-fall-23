ff41c0e9edb8184dbb1cf551a5078f52d6449f22
Aviv Cohen <avivcohen31@gmail.com>
2023-09-29 14:59:31 -0400

    Add event handler for Next Question button & feth new question from API

--- src/App.js
+++ src/App.js
@@ -14,6 +14,20 @@ function App() {
     setSelectedAnswer(selection);
   };
 
+  const nextQuestion = async () => {
+    try {
+      let response = await fetch('https://opentdb.com/api.php?amount=1&category=9&type=multiple');
+      let body = await response.json();
+      console.log('Success', body);
+      if (body.results) {
+        setQuestionData(body.results[0]);
+        setSelectedAnswer(null);
+      }
+    } catch(error) {
+      console.log('Error:', error);
+    }
+  };
+
   let card;
 
   if (selectedAnswer) {
@@ -41,8 +55,11 @@ function App() {
     <div className="w-100 my-5 d-flex justify-content-center align-items-center">
       <div style={{ maxWidth: "45%" }}>
         <h1 className="text-center">Trivia App</h1>
-        <button className="btn btn-success">Next Question</button>
+        <button className="btn btn-success" onClick={nextQuestion}>
+          Next Question
+        </button>
         {card}
+        
       </div>
     </div>
   );
