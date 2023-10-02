1b934f0528ff8c385f2f0a64686fd265980902d4
matteoSaputo <matteosaputo1201@gmail.com>
2023-10-02 17:16:21 -0400

    Finished

--- src/components/AnswerButton.js
+++ src/components/AnswerButton.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 function AnswerButton({ value, handleClick }) {
+  
   return (
     <button className="btn btn-primary w-100 mt-4" onClick={handleClick}>
       {value}
